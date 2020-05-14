581c2eb74c42302c20e7a367be030e344fd31a10
Nuno Pássaro <nunomcpassaro@gmail.com>
2020-05-14 15:42:59 +0100

    :sparkles: replace months number per initials

--- app/helpers/greens_helper.rb
+++ app/helpers/greens_helper.rb
@@ -5,4 +5,16 @@ module GreensHelper
       .map
       .with_index { |name, i| [ i, name ]  }
   end
+
+  def period_to_string(period)
+    period
+      .compact
+      .map {|m| first_letter_month(m) }
+      .join('')
+  end
+
+  private
+    def first_letter_month(month)
+      I18n.t('date.month_names')[month][0]
+    end
 end
